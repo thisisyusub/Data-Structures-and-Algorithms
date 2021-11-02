715fea44a8876641c3ab364c20e6c0c7499cef7a
thisisyusub <kanan.yusub@gmail.com>
2021-11-02 06:23:45 +0400

    doc for singly linked list added

--- Lists/singly_linked_list.dart
+++ Lists/singly_linked_list.dart
@@ -1,33 +1,8 @@
-void main() {
-  final singlyLinkedList = SinglyLinkedList<int>();
-
-  print('Adding First ---------');
-  singlyLinkedList.addFirst(1);
-  singlyLinkedList.addFirst(2);
-  singlyLinkedList.addFirst(3);
-
-  print(singlyLinkedList);
-  print(singlyLinkedList.first());
-  print(singlyLinkedList.last());
-  print(singlyLinkedList.size);
-
-  print('Adding Last ---------');
-  singlyLinkedList.addLast(4);
-  print(singlyLinkedList);
-  print(singlyLinkedList.first());
-  print(singlyLinkedList.last());
-  print(singlyLinkedList.size);
-
-  print('Removing First ---------');
-  singlyLinkedList.removeFirst();
-  print(singlyLinkedList);
-  print(singlyLinkedList.first());
-  print(singlyLinkedList.last());
-  print(singlyLinkedList.size);
-}
-
 class _Node<E> {
+  /// reference to the element stored at this node
   E? element;
+
+  /// reference to the subsequent node in the list
   _Node<E>? next;
 
   _Node([this.element, this.next]);
@@ -40,50 +15,71 @@ class _Node<E> {
   }
 }
 
+/// Implementation of Singly Linked List
 class SinglyLinkedList<E> {
+  /// head node of the list (or null if empty)
   _Node<E>? _head = null;
+
+  /// head node of the list (or null if empty)
   _Node<E>? _tail = null;
+
+  /// head node of the list (or null if empty)
   int _size = 0;
 
   int get size => _size;
 
   bool get isEmpty => _size == 0;
 
+  /// returns (but does not remove) the first element
   E? first() => isEmpty ? null : _head!.element;
 
+  /// returns (but does not remove) the first element
   E? last() => isEmpty ? null : _tail!.element;
 
+  /// adds element e to the front of the list
   void addFirst(E value) {
+    ///adds element e to the front of the list
     _head = _Node<E>(value, _head);
 
     if (_size == 0) {
+      /// special case: new node becomes tail also
       _tail = _head;
     }
 
     _size++;
   }
 
+  /// special case: new node becomes tail also
   void addLast(E value) {
+    /// node will eventually be the tail
     final newNode = _Node<E>(value, null);
 
     if (isEmpty) {
+      /// special case: previously empty list
       _head = newNode;
     } else {
+      /// new node after existing tail
       _tail!.next = newNode;
     }
 
+    /// new node after existing tail
     _tail = newNode;
     _size++;
   }
 
+  /// removes and returns the first element
   E? removeFirst() {
+    /// removes and returns the first element
     if (isEmpty) return null;
 
     final first = _head!.element;
+
+    /// will become null if list had only one node
     _head = _head!.next;
     _size--;
 
     if (_size == 0) {
+      /// special case as list is now empty
       _tail = null;
     }
 
---
+++ Stack & Queue (Deque)/list_stack.dart
@@ -0,0 +1,3 @@
+void main() {}
+
+class ListStack<E> {}
